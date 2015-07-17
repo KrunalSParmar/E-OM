dc0d31496819039d4026af54cfce9393ac72b148
200270462 <200270462@student.georgianc.on.ca>
2015-07-16 23:24:55 -0400

    re-commit

---
+++ OnlinExpenseManager/register.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/OEM.Master" AutoEventWireup="true" CodeBehind="register.aspx.cs" Inherits="OnlinExpenseManager.register" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Register</h1>
+    <h5>All fields are required</h5>
+
+    <div class="form-group-lg">
+        <asp:label id="lblStatus" runat="server" cssclass="label label-danger" />
+    </div>
+    <div class="form-group">
+        <label for="txtEmail" class="col-sm-2">Email:</label>
+        <asp:textbox id="txtEmail" runat="server" />
+    </div>
+    <div class="form-group">
+        <label for="txtPassword" class="col-sm-2">Password:</label>
+        <asp:textbox id="txtPassword" runat="server" textmode="password" />
+    </div>
+    <div class="form-group">
+        <label for="txtConfirm" class="col-sm-2">Confirm Password:</label>
+        <asp:textbox id="txtConfirm" runat="server" textmode="password" />
+        <asp:comparevalidator runat="server" operator="Equal" controltovalidate="txtPassword"
+            controltocompare="txtConfirm" cssclass="label label-danger" errormessage="Passwords must match" />
+    </div>
+     <div class="form-group">
+        <label for="txtFName" class="col-sm-2">First Name:</label>
+        <asp:textbox id="txtFName" runat="server" />
+    </div>
+    <div class="form-group">
+        <label for="txtLName" class="col-sm-2">Last Name:</label>
+        <asp:textbox id="txtLName" runat="server" />
+    </div>
+    <div class="form-group">
+        <label for="txtPhone" class="col-sm-2">Phone:</label>
+        <asp:textbox id="txtPhone" runat="server" />
+    </div>
+    <div class="form-group">
+        <label for="txtCredit" class="col-sm-2">Credit Account Balance:</label>
+        <asp:textbox id="txtCredit" runat="server" />
+    </div>
+    <div class="form-group">
+        <label for="txtDebit" class="col-sm-2">Debit Account Balance:</label>
+        <asp:textbox id="txtDebit" runat="server" />
+    </div>
+    <div class="col-sm-offset-2">
+        <asp:button id="btnRegister" runat="server" text="Register" cssclass="btn btn-primary" 
+            OnClick="btnRegister_Click" />
+    </div>
+</asp:Content>
---
+++ OnlinExpenseManager/register.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//ER References
+using OnlinExpenseManager.Models;
+using System.Web.ModelBinding;
+
+
+//identity package references
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace OnlinExpenseManager
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Default UserStore constructor uses the default connection string named: DefaultConnection
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            var email = new IdentityUser() { UserName = txtEmail.Text };
+            email.PhoneNumber = txtPhone.Text;
+            email.Email = txtEmail.Text;
+
+            IdentityResult result = manager.Create(email, txtPassword.Text);
+
+            if (result.Succeeded)
+            {
+                lblStatus.Text = string.Format("User {0} was created successfully!", email.UserName);
+                lblStatus.CssClass = "label label-success";
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(email, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                using (ExpMgmtEntities db = new ExpMgmtEntities())
+                    {
+                        User u = new User();
+                        Account a = new Account();
+                        u.FName = txtFName.Text;
+                        u.LName = txtLName.Text;
+                        u.Email = txtEmail.Text;
+                        u.Phone = txtPhone.Text;
+                        a.CreditBalance = Convert.ToDouble(txtCredit.Text);
+                        a.DebitBalance = Convert.ToDouble(txtDebit.Text);
+                        
+                        //Add user and account to database
+                        db.Users.Add(u);
+                        db.Accounts.Add(a);
+
+                        //run the update or insert
+                        db.SaveChanges();
+                    }
+                 //redirect to the updated students page
+                 Response.Redirect("home.aspx");
+            }
+            else
+            {
+                lblStatus.Text = result.Errors.FirstOrDefault();
+                lblStatus.CssClass = "label label-danger";
+            }
+        }
+    }
+}
